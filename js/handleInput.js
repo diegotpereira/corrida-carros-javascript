e13eed716abce2dabdfbf722c8f37084cb983aea
diegotpereira <diegoteixeirapereira@hotmail.com>
2023-07-23 00:39:54 -0300

    alteração metodo ouvinte de eventos.

--- js/handleInput.js
+++ js/handleInput.js
@@ -1,73 +1,74 @@
 class HandleInput {
+
     constructor() {
-      const fullScreenBtn = document.getElementById('fullScreenBtn');
-      fullScreenBtn.addEventListener('click', (event) => HandleInput.toggleFullScreen(event));
-      const pauseButton = document.querySelector('#botaoPausar');
-      pauseButton.addEventListener('click', (event) => this.pause(event));
-      const moveButtons = document.querySelectorAll('button');
+
+      // Adiciona ouvintes de eventos para os eventos 'keydown', 'keyup' e 'keypress'
       window.addEventListener('keydown', (event) => this.handler(event));
       window.addEventListener('keyup', (event) => this.handler(event));
       window.addEventListener('keypress', (event) => this.handler(event));
-      moveButtons.forEach((button) => {
-        if (button.name !== 'fullscreenbtn') {
-          button.addEventListener('contextmenu', (event) => event.preventDefault());
-          button.addEventListener('touchstart', (event) => this.handler(event));
-          button.addEventListener('touchend', (event) => this.handler(event));
-        }
-      });
   
+      // Cria um objeto vazio para mapear as teclas pressionadas
       this.map = {};
+
+      // Cria um objeto para mapear as teclas pressionadas que 
+      // precisam de verificação de pressionamento único
       this.mapPress = { p: true, enter: false };
     }
   
-    pause(e) {
-      const pauseBtn = document.querySelector('#botaoPausar');
-      pauseBtn.classList.toggle('off');
-      if (!window.navigator.maxTouchPoints && e.type !== 'keypress') {
-        this.mapPress.p = !this.mapPress.p;
-      }
-    }
   
     /**
-     *
+     * Trata o evento de entrada de teclado
      * @param {KeyboardEvent} event
      */
     handler(event) {
+
+      // Verifica se o evento é do tipo 'keypress'
       if (event.type === 'keypress') {
+
+        // Obtém a tecla pressionada em letras minúsculas
         const key = event.key.toLowerCase();
+
+        // Verifica se o evento não é um evento de repetição
         if (!event.repeat) {
+
+          // Define o estado da tecla no objeto 'mapPress' para 
+          // indicar se ela está pressionada ou não
           this.mapPress[key] = !this.mapPress[key];
+
+          // Verifica se a tecla pressionada é 'p' e pausa o jogo se for
           if (event.key === 'p') {
             this.pause(event);
           }
         }
+
       } else if (event.type === 'keyup' || event.type === 'keydown') {
-        const key = event.key.toLowerCase();
-        this.map[key] = event.type === 'keydown';
+
+        // // Obtém a tecla pressionada em letras minúsculas
+        // const key = event.key.toLowerCase();
+
+        // // Define o estado da tecla no objeto 'map' para indicar se 
+        // // ela está pressionada ou não
+        // this.map[key] = event.type === 'keydown';
       }
-      if ((event.target.name !== 'p') && (event.type === 'touchstart' || event.type === 'touchend')) {
-        const key = event.target.name;
-        this.map[key] = event.type === 'touchstart';
-        this.mapPress[key] = event.type === 'touchend';
-      }
-      if (event.target.name === 'p' && event.type === 'touchend') {
-        this.mapPress.p = !this.mapPress.p;
-      }
-    }
-  
-    isKeyDown(key) {
-      return Boolean(this.map[key.toLowerCase()]);
-    }
-  
-    static toggleFullScreen() {
-      const gameContainer = document.querySelector('.container');
-      if (!document.fullscreenElement) {
-        gameContainer.requestFullscreen().catch((err) => {
-          alert(`Error, can't enable full-screen ${err.message}`);
-        });
-      } else {
-        document.exitFullscreen();
-      }
+
+      // // Verifica se o evento é do tipo 'touchstart' ou 'touchend' e a tecla não é 'p'
+      // if ((event.target.name !== 'p') && (event.type === 'touchstart' || event.type === 'touchend')) {
+
+      //   // Obtém o nome do alvo do evento touch
+      //   const key = event.target.name;
+
+      //   // Define o estado da tecla no objeto 'map' com base no tipo de evento touch
+      //   this.map[key] = event.type === 'touchstart';
+
+      //   // Define o estado da tecla no objeto 'mapPress' com base no tipo de evento touch
+      //   this.mapPress[key] = event.type === 'touchend';
+      // }
+
+      // // Verifica se o evento é do tipo 'touchend' e a tecla é 'p', alterando o 
+      // // estado do pressionamento do botão 'p'
+      // if (event.target.name === 'p' && event.type === 'touchend') {
+      //   this.mapPress.p = !this.mapPress.p;
+      // }
     }
   }
   
